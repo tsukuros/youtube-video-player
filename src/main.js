d6002494543b4ef2e313db3686d7a600ed8ea4c6
Oleksandr Tsukur <tsukuros@gmail.com>
2020-05-11 14:50:07 +0300

    02: Side Menu Updates for Youtube 2020

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-const desktopBreakpoint = 1200;
-const sideMenu = document.getElementById("side-menu");
+// const desktopBreakpoint = 1200;
+// const sideMenu = document.getElementById("side-menu");
 
-function onResize() {
-  const winWidth = window.innerWidth;
-  if (winWidth > desktopBreakpoint - 1) {
-    // desktop
-    sideMenu.classList.remove("mobile");
-  } else {
-    // mobile
-    sideMenu.classList.add("mobile");
-  }
-}
+// function onResize() {
+//   const winWidth = window.innerWidth;
+//   if (winWidth > desktopBreakpoint - 1) {
+//     // desktop
+//     sideMenu.classList.remove("mobile");
+//   } else {
+//     // mobile
+//     sideMenu.classList.add("mobile");
+//   }
+// }
 
-window.addEventListener("resize", onResize);
-onResize();
+// window.addEventListener("resize", onResize);
+// onResize();
