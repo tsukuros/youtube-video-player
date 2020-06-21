257e4906142e170a1055004e86d2a539143c261b
Oleksandr Tsukur <tsukuros@gmail.com>
2020-06-21 09:57:41 +0300

    Play/pause icon with animation

--- src/js/video-player.js
+++ src/js/video-player.js
@@ -2,6 +2,9 @@
   function VideoPlayer(elem) {
     this.containerElem = elem;
     this.videoElem = this.containerElem.querySelector('[data-player="video"]');
+    this.playPauseIconElem = this.containerElem.querySelector(
+      '[data-player="play-pause-icon"]'
+    );
 
     // events
     this.videoElem.addEventListener("click", this.toggle.bind(this));
@@ -11,13 +14,24 @@
     constructor: VideoPlayer,
     play: function () {
       this.videoElem.play();
+      this.playPauseIconElem.classList.add("play-mode");
+      this.playPauseIconElem.style.display = "";
+      window.setTimeout(this.hidePlayPauseIcon.bind(this), 500);
     },
     pause: function () {
       this.videoElem.pause();
+      this.playPauseIconElem.classList.add("pause-mode");
+      this.playPauseIconElem.style.display = "";
+      window.setTimeout(this.hidePlayPauseIcon.bind(this), 500);
     },
     toggle: function () {
       if (this.videoElem.paused) this.play();
       else this.pause();
+    },
+    hidePlayPauseIcon: function () {
+      this.playPauseIconElem.classList.remove("play-mode");
+      this.playPauseIconElem.classList.remove("pause-mode");
+      this.playPauseIconElem.style.display = "none";
     },
   };
 
